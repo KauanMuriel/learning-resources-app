635080bfac3f1ef0dba9b2ee72f5646bc5ccf71e
Kauan Muriel Rossi da Silva <kauan.silva@benner.com.br>
2023-02-06 09:46:33 -0300

    resource item component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
     <ul>
-        
+        <learning-resource v-for="resource in storedResources"
+        :key="resource.id"
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+        ></learning-resource>
     </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+    components: {
+        'learning-resource': LearningResource
+    },
     data() {
         return {
             storedResources: [
