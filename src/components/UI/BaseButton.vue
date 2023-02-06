da763dd6f9a69efb6b8ba90ed772962cf24aabac
Kauan Muriel Rossi da Silva <kauan.silva@benner.com.br>
2023-02-06 10:41:18 -0300

    Base button and replacements

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button :type="type" :class="mode">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+button {
+    padding: 0.75rem 1.5rem;
+    background-color: rgb(172, 39, 16);
+    border: 1px solid rgb(172, 39, 16);
+    border: 0;
+    color: white;
+    cursor: pointer;
+    font-size: 16px;
+    font-family: inherit;
+}
+
+button:hover, button:active {
+    background-color: rgb(143, 33, 14);
+    border-color: rgb(143, 33, 14);
+}
+
+.flat {
+    background-color: transparent;
+    color: rgb(172, 39, 16);
+}
+
+.flat:hover, .flat:active {
+    background-color: transparent;
+    color: rgb(143, 33, 14);
+}
+</style>
--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -16,7 +16,7 @@ div {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     border-radius: 12px;
     max-width: 40rem;
-    padding: 1%;
+    padding: 2%;
     margin: 2rem auto;
 }
 </style>
