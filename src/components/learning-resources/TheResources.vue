03fd7480c6055d1b6b5f94b30d7c846181962b60
Kauan Muriel Rossi da Silva <kauan.silva@benner.com.br>
2023-02-06 11:44:54 -0300

    Add resource functionality

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,9 +1,12 @@
 <template>
     <base-card>
-        <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+        <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored
+            Resources</base-button>
         <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
     </base-card>
-    <component :is="selectedTab"></component>
+    <keep-alive>
+        <component :is="selectedTab"></component>
+    </keep-alive>
 </template>
 
 <script>
@@ -44,12 +47,23 @@ export default {
     },
     provide() {
         return {
-            resources: this.storedResources
+            resources: this.storedResources,
+            addResource: this.addResource
         }
     },
     methods: {
         setSelectedTab(tab) {
             this.selectedTab = tab;
+        },
+        addResource(title, description, url) {
+            const newResource = {
+                id: new Date().toISOString(),
+                title: title,
+                description: description,
+                link: url
+            }
+            this.storedResources.push(newResource);
+            this.selectedTab = 'stored-resources';
         }
     }
 }
